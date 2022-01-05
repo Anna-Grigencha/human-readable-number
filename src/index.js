baa7c0d8520f7d105a3b379caf6172b145a9b151
Anna-Grigencha <grigencha10@gmail.com>
2022-01-05 23:58:38 +0300

    feat: modified index.js

--- src/index.js
+++ src/index.js
@@ -1,88 +1,114 @@
 module.exports = function toReadable(number) {
-    const arr = (x) => Array.from(x);
-    const num = (x) => Number(x) || 0;
-    const str = (x) => String(x);
-    const isEmpty = (xs) => xs.length === 0;
-    const take = (n) => (xs) => xs.slice(0, n);
-    const drop = (n) => (xs) => xs.slice(n);
-    const reverse = (xs) => xs.slice(0).reverse();
-    const comp = (f) => (g) => (x) => f(g(x));
-    const not = (x) => !x;
-    const chunk = (n) => (xs) =>
-        isEmpty(xs) ? [] : [take(n)(xs), ...chunk(n)(drop(n)(xs))];
+    number = number.toString();
 
-    let numToWords = (n) => {
-        let a = [
-            "",
-            "one",
-            "two",
-            "three",
-            "four",
-            "five",
-            "six",
-            "seven",
-            "eight",
-            "nine",
-            "ten",
-            "eleven",
-            "twelve",
-            "thirteen",
-            "fourteen",
-            "fifteen",
-            "sixteen",
-            "seventeen",
-            "eighteen",
-            "nineteen",
-        ];
-
-        let b = [
-            "",
-            "",
-            "twenty",
-            "thirty",
-            "forty",
-            "fifty",
-            "sixty",
-            "seventy",
-            "eighty",
-            "ninety",
-        ];
-
-        let g = [
-            "",
-            "thousand",
-            "million",
-            "billion",
-            "trillion",
-            "quadrillion",
-            "quintillion",
-            "sextillion",
-            "septillion",
-            "octillion",
-            "nonillion",
-        ];
-
-        let makeGroup = ([ones, tens, huns]) => {
-            return [
-                num(huns) === 0 ? "" : a[huns] + " hundred ",
-                num(ones) === 0 ? b[tens] : (b[tens] && b[tens] + " ") || "",
-                a[tens + ones] || a[ones],
-            ].join("");
-        };
-
-        let thousand = (group, i) =>
-            group === "" ? group : `${group} ${g[i]}`;
-
-        if (typeof n === "number") return numToWords(str(n));
-        else if (n === "0") return "zero";
-        else
-            return comp(chunk(3))(reverse)(arr(n))
-                .map(makeGroup)
-                .map(thousand)
-                .filter(comp(not)(isEmpty))
-                .reverse()
-                .join(" ")
-                .trim();
+    let getUnits = (number) => {
+        switch (number) {
+            case "0":
+                return "zero";
+            case "1":
+                return "one";
+            case "2":
+                return "two";
+            case "3":
+                return "three";
+            case "4":
+                return "four";
+            case "5":
+                return "five";
+            case "6":
+                return "six";
+            case "7":
+                return "seven";
+            case "8":
+                return "eight";
+            case "9":
+                return "nine";
+            default:
+                return null;
+        }
     };
-    return numToWords(number);
+
+    let getDozens = (number) => {
+        switch (number) {
+            case "1":
+                return "one";
+            case "2":
+                return "twenty";
+            case "3":
+                return "thirty";
+            case "4":
+                return "forty";
+            case "5":
+                return "fifty";
+            case "6":
+                return "sixty";
+            case "7":
+                return "seventy";
+            case "8":
+                return "eighty";
+            case "9":
+                return "ninety";
+            default:
+                return null;
+        }
+    };
+
+    let getFromElevenToNineteen = (number) => {
+        switch (number) {
+            case "0":
+                return "ten";
+            case "1":
+                return "eleven";
+            case "2":
+                return "twelve";
+            case "3":
+                return "thirteen";
+            case "4":
+                return "fourteen";
+            case "5":
+                return "fifteen";
+            case "6":
+                return "sixteen";
+            case "7":
+                return "seventeen";
+            case "8":
+                return "eighteen";
+            case "9":
+                return "nineteen";
+            default:
+                return null;
+        }
+    };
+
+    if (number.length === 1) {
+        return getUnits(number);
+    } else if (number === "10") {
+        return "ten";
+    } else if (number.length === 2 && number[1] === "0") {
+        return getDozens(number[0]);
+    } else if (number.length === 2 && number[0] === "1") {
+        return getFromElevenToNineteen(number[1]);
+    } else if (number.length === 2) {
+        return getDozens(number[0]) + " " + getUnits(number[1]);
+    } else if (number.length === 3 && number[1] === "0" && number[2] === "0") {
+        return getUnits(number[0]) + " hundred";
+    } else if (number.length === 3 && number[1] === "0") {
+        return getUnits(number[0]) + " hundred " + getUnits(number[2]);
+    } else if (number.length === 3 && number[1] === "1") {
+        return (
+            getUnits(number[0]) +
+            " hundred " +
+            getFromElevenToNineteen(number[2])
+        );
+    } else if (number.length === 3 && number[2] === "0") {
+        return getUnits(number[0]) + " hundred " + getDozens(number[1]);
+    } else if (number.length === 3) {
+        return (
+            getUnits(number[0]) +
+            " hundred " +
+            getDozens(number[1]) +
+            " " +
+            getUnits(number[2])
+        );
+    }
 };
